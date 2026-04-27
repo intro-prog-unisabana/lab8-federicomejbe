bd5cf79c96bcb0860ef634f66fed3d4622cdb895
Federico Mejia <federicomejbe@unisabana.edu.co>
2026-04-27 10:03:25 -0500

    trail 1

--- question1.py
+++ question1.py
@@ -4,3 +4,17 @@ Implementa una CLI que calcule carga por punto de soporte.
 """
 
 # TODO: Implementar según README.md
+import sys 
+try:
+    total_load = float(sys.argv[1])
+    num_supports = float(sys.argv[2])
+
+    if num_supports == 0:
+        print("Error: Cannot divide by zero! Supports must be greater than zero.")
+    else:
+        load_per_support = total_load / num_supports
+        print(f"Load per support point: {load_per_support:.2f} N")
+
+except:
+    print("Error: Invalid input! Enter numeric values only.")
+    
